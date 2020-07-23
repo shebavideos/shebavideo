1e98fe54f11352832be454055175fba02b97f2ec
Kebalepile <pimpabutterfly86@gmail.com>
2020-07-23 20:58:46 +0200

    updated reducer switch of WATCH && WATCH next.

--- src/context/videos/Reducer.js
+++ src/context/videos/Reducer.js
@@ -16,25 +16,32 @@ export default function reducer(state = {
     watchNext: null,
     videos: []
 }, action) {
-    
+
     switch (action.type) {
+
         case WATCH:
+
+            var index = state.videos.findIndex(element => element.id === action.payload);
+
             return {
                 ...state,
-                watch: state.videos[action.payload] || null
+                watch: state.videos[index] || null
             }
         case WATCHNEXT:
+
+            var index = state.videos.findIndex(element => element.id === action.payload);
+
             return {
                 ...state,
-                watchNext: state.videos[action.payload] || null
+                watchNext: state.videos[index] || null
             }
         case UPLOADED:
-            
-            const alreadyUploaded = state.videos.reduce((acc,cur) => {
-                 acc.push(cur.name);
-                 return acc;
-            },[]);
-            
+
+            const alreadyUploaded = state.videos.reduce((acc, cur) => {
+                acc.push(cur.name);
+                return acc;
+            }, []);
+
             const uploads = action.payload.filter(object => !alreadyUploaded.includes(object.name));
 
             return {
@@ -42,11 +49,13 @@ export default function reducer(state = {
                 videos: [...state.videos, ...uploads]
             }
         case REMOVE:
+
             return {
                 ...state,
                 videos: state.videos.filter(video => video.id !== action.payload)
             }
         default:
+
             return state;
     }
 }
